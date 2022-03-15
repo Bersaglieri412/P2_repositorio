4a68577c119e826054ab8ea039f244007f5bf3e7
Francisco-2002-java <101673971+Francisco-2002-java@users.noreply.github.com>
2022-03-15 22:42:14 +0100

    main.java practica2
    practica 2, main.java documentado

--- src/main.java
+++ src/main.java
@@ -1,24 +1,47 @@
+/*******************************************************************
+* Nombre de la clase: main
+* Descripción de la clase: Será la encargada de ejecutar el programa.
+*******************************************************************/
 
 public class main {
 
+	/*******************************************************************
+	* Nombre del método: main 
+	* Descripción del método: Su función es ejecutar los métodos necesarios para el programa, en este caso menu().
+	* Argumentos de llamada: -
+	* Valor de retorno: -
+	* Archivos requeridos: -
+	* Lista de excepciones:-
+	*******************************************************************/
+	
 	public static void main(String[] args) {
 		menu();
 		
 	}
+	
+	/*******************************************************************
+	* Nombre del método: menu 
+	* Descripción del método: Permite selecionar una ejecución, dando a elegir como opcines: crear una matriz de dimension potencia de 2
+	* Argumentos de llamada: -
+	* Valor de retorno: -
+	* Archivos requeridos: -
+	* Lista de excepciones:-
+	*******************************************************************/
+	
 	public static void menu() {
-		System.out.println("Elija una opci�n del men�:\n1.M�todo entero\n2.C�lculo complejidad");
+		System.out.println("Elija una opción del menú:\n1.Método entero\n2.Cálculo complejidad");
 		int i=leer.entero();
 		switch(i) {
 		case 1:
-			System.out.println("Se ense�ar� el funcionamiento completo del m�todo");
+			System.out.println("Se enseñará el funcionamiento completo del método");
 			compresionImagen.compresion();
 			break;
 		case 2:
-			System.out.println("Se calcular� la complejidad");
+			System.out.println("Se calculará la complejidad");
 			complejidad.calculo();
 			break;
 		default:
-			System.out.println("La opci�n no corresponde con ninguna propuesta, se cerrar� el men�");
+			System.out.println("La opción no corresponde con ninguna propuesta, se cerrará el menú");
 			break;
 		}
 	}
